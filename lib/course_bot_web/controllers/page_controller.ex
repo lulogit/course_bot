18525f17b45e8b409bc341f9c4a7ce16d0ecc034
lulogit <luca1.lodi@mail.polimi.it>
2019-02-12 14:28:33 +0100

    added intro bot page + bubbles.js

--- lib/course_bot_web/controllers/page_controller.ex
+++ lib/course_bot_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule CourseBotWeb.PageController do
   use CourseBotWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "intro.html"
   end
 end
--- lib/course_bot_web/endpoint.ex
+++ lib/course_bot_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule CourseBotWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :course_bot, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt Bubbles.js setup.css typing.css says.css reply.css input.css)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
